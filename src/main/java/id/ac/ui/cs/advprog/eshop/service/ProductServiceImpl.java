81e622292b10e4ab433ddf5788265066acf04c8a
Yufanjonni <muhammad.yufan@ui.ac.id>
2026-02-24 13:34:55 +0700

    remove public from interface and declare everything as final

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,36 +10,40 @@ import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService{
-    @Autowired
-    private ProductRepository productRepository;
+
+    final private ProductRepository productRepository;
+
+    public ProductServiceImpl(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
-    public Product create(Product product){
+    public Product create(final Product product){
         productRepository.create(product);
         return product;
     }
 
     @Override
     public List<Product> findAll(){
-        Iterator<Product> productIterator = productRepository.findAll();
-        List<Product> allProduct = new ArrayList<>();
+        final Iterator<Product> productIterator = productRepository.findAll();
+        final List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
 
     @Override
-    public Product edit(Product newProduct){
+    public Product edit(final Product newProduct){
         productRepository.edit(newProduct);
         return newProduct;
     }
 
     @Override
-    public Product findById(String productId) {
+    public Product findById(final String productId) {
         return productRepository.findById(productId);
     }
 
     @Override
-    public void delete(Product product){
+    public void delete(final Product product){
         productRepository.delete(product);
     }
 }
