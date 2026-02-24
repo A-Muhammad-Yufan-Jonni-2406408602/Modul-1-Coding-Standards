6492bcf314a490d8b0392b15757f467be1b98687
Yufanjonni <muhammad.yufan@ui.ac.id>
2026-02-24 13:18:44 +0700

    adding contructor and declare every field as final in ProductController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -13,38 +13,41 @@ import java.util.List;
 @RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
 
     @GetMapping("/create")
-    public String createProductPage(Model model){
-        Product product = new Product();
+    public String createProductPage(final Model model){
+        final Product product = new Product();
         model.addAttribute("product", product);
         return "createProduct";
     }
 
     @PostMapping("/create")
-    public String createProductPost(@ModelAttribute Product product, Model model){
+    public String createProductPost(@ModelAttribute final Product product, final Model model){
         service.create(product);
         return "redirect:list";
     }
 
     @GetMapping("/list")
-    public String productListPage(Model model){
-        List<Product> allProduct = service.findAll();
+    public String productListPage(final Model model){
+        final List<Product> allProduct = service.findAll();
         model.addAttribute("products", allProduct);
         return "productList";
     }
 
     @GetMapping("/edit/{productId}")
-    public String editProductPage(@PathVariable String productId, Model model){
-        Product oldProduct = service.findById(productId);
+    public String editProductPage(@PathVariable final String productId, final Model model){
+        final Product oldProduct = service.findById(productId);
         model.addAttribute("product", oldProduct);
         return "editProduct";
     }
 
     @PostMapping("/edit")
-    public String editProductPost(@ModelAttribute Product newProduct, Model model){
+    public String editProductPost(@ModelAttribute final Product newProduct, final Model model){
         service.edit(newProduct);
         return "redirect:list";
     }
